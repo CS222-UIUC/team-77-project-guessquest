f673992358e40ebfa01cff2ce816f7174d158334
Jordan Smith <jordansmith@wirelessprv-10-192-251-230.near.illinois.edu>
2025-03-07 20:30:32 -0600

    Changes to games-models

--- guessquest/games/models.py
+++ guessquest/games/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TemperatureGame(models.Model):
+    total_score = models.IntegerField(default=0)
+    questions_left = models.IntegerField(default=5)
+    
+        
+class TemperatureQuestion(models.Model):
+    game = models.ForeignObject(TemperatureGame, related_name="questions")
+    city = models.CharField(max_length=50)
+    user_guess = models.FloatField(null=True, blank=True)
+    actual_temparature = models.FloatField(null=False, blank=False)
+    time_limit = models.IntegerField(default=30) # 30 seconds
+    
+    
