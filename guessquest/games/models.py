eb4786e26a98047162dbdabe3df93363d187aeb8
Jordan Smith <jordansmith@Mac.home>
2025-03-27 13:25:19 -0500

    Added a gitignore

--- guessquest/games/models.py
+++ guessquest/games/models.py
@@ -9,7 +9,7 @@ class Player(models.Model):
     
     def update_high_score(self, score):
         if score > self.high_score:
-            self.score = score
+            self.high_scores = score
         
     
     # returns username as a string
